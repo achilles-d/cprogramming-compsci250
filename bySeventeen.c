4ade55dbc8efcb9424ebdbd92251e4bc9047dfa0
Achilles Dabrowski <ajd66@duke.edu>
2020-01-25 23:49:26 +0000

    attempt 2

--- bySeventeen.c
+++ bySeventeen.c
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[]){
     int n = atoi(argv[1]);
     int i = 1;
-    int multiple = 17;
+    int multiple = 0;
     for(i = 1; i <= n; i++){
-        multiple *= i; 
-        printf("%d", multiple);
+        multiple *= i * 17; 
+        printf("%d \n", multiple);
     }
     return EXIT_SUCCESS;   
 }
